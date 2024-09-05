4ee39c649db4f105c00ff7412a6afe1f12edfd96
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-05 09:54:23 +0530

    chat app

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const User = require('../models/User');
+
+exports.googleAuth = (passport) => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        const newUser = new User({
+          googleId: profile.id,
+          email: profile.emails[0].value,
+        });
+        await newUser.save();
+        done(null, newUser);
+      }
+    )
+  );
+};
+
+exports.protect = (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Not authorized' });
+  }
+};
