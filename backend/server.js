1c42cd9c7a9940e2c8c78e903e823a164af0b48f
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-05 10:04:12 +0530

    chat app

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const authRoutes = require('./routes/authRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const { googleAuth } = require('./middlewares/authMiddleware');
 
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
